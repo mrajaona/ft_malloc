44a939dba791b71cea9647ec32c75d9ca9401ada
maryse <rajaon_m@naver.com>
2019-02-11 14:50:10 +0100

    main de test qui vuet pas utiliser ma lib

--- rendu/src/free/free.c
+++ rendu/src/free/free.c
@@ -2,10 +2,12 @@
 
 void	free(void *ptr)
 {
+	t_identifier	*id;
+
+	write(1, "e", 1);
 	if (ptr == NULL)
 		return ;
-	/*
-	if (LARGE)
-		munmap(addr, size + sizeof(t_identifier));
-	*/
+	id = (t_identifier *)(ptr - sizeof(t_identifier));
+	if (id->type == LARGE)
+		munmap(id->addr, id->size + sizeof(t_identifier));
 }
--- rendu/src/show/show_mem_alloc.c
+++ rendu/src/show/show_mem_alloc.c
@@ -2,4 +2,5 @@
 
 void	show_mem_alloc()
 {
+	write(1, "show_mem_alloc\n", 15);
 }
