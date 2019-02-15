0f54544a2206d1e793df32b7f2c81e2eca686799
maryse <rajaon_m@naver.com>
2019-02-15 13:31:49 +0100

    bug revnbr

--- rendu/main.c
+++ rendu/main.c
@@ -17,9 +17,9 @@ int	main(int ac, char **av)
 	show_mem_alloc();
 	write(1, "1 --\n", 5);
 
-	a = malloc(1 << 6);
-	b = malloc(1 << 7);
-	c = malloc(1 << 8);
+	a = malloc(1 << 1);
+	b = malloc(1 << 1);
+	c = malloc(1 << 1);
 
 	if (!a) write(1, "a is NULL\n", 10);
 	if (!b) write(1, "b is NULL\n", 10);
@@ -31,60 +31,5 @@ int	main(int ac, char **av)
 	free(a); a = NULL;
 	free(b); b = NULL;
 	free(c); c = NULL;
-
-	a = malloc(1 << 4);
-	b = malloc(1 << 4);
-	c = malloc(1 << 4);
-
-	if (!a) write(1, "a is NULL\n", 10);
-	if (!b) write(1, "b is NULL\n", 10);
-	if (!c) write(1, "c is NULL\n", 10);
-
-	show_mem_alloc();
-	write(1, "3 --\n", 5);
-
-	free(a); a = NULL;
-	free(b); b = NULL;
-	free(c); c = NULL;
-
-	a = malloc(1 << 12);
-	b = malloc(1 << 12);
-	c = malloc(1 << 12);
-
-	if (!a) write(1, "a is NULL\n", 10);
-	if (!b) write(1, "b is NULL\n", 10);
-	if (!c) write(1, "c is NULL\n", 10);
-
-	show_mem_alloc();
-	write(1, "4 --\n", 5);
-
-	free(a); a = NULL;
-	free(b); b = NULL;
-	free(c); c = NULL;
-
-	a = realloc(a, 1 << 2);
-	b = realloc(b, 1 << 10);
-	c = realloc(c, 0); // free
-
-	if (!a) write(1, "a is NULL\n", 10);
-	if (!b) write(1, "b is NULL\n", 10);
-	if (!c) write(1, "c is NULL\n", 10);
-
-	if (realloc(a + 5, 1 << 2) == NULL)
-		write(1, "-N\n", 3);
-
-	show_mem_alloc();
-	write(1, "5 --\n", 5);
-
-	free(a); a = NULL;
-	free(b); b = NULL;
-	free(c); c = NULL;
-
-	if (!a) write(1, "a is NULL\n", 10);
-	if (!b) write(1, "b is NULL\n", 10);
-	if (!c) write(1, "c is NULL\n", 10);
-
-	show_mem_alloc();
-	write(1, "6 --\n", 5);
 	return (0);
 }
