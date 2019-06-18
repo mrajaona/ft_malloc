b331ab6754503d282d5e866197433ad79fbb796b
Maryse RAJAONARY <mrajaona@e3r5p9.42.fr>
2019-06-18 10:57:02 +0200

    ...

--- rendu/inc/memory.h
+++ rendu/inc/memory.h
@@ -2,28 +2,29 @@
 # define MEMORY_H
 
 /*
-** struct s_memory
-** size : total size with header
+** size : available size
 ** isfree : status
+** addr : start of available memory
 */
 
-typedef struct	s_memory
+typedef struct	s_info
 {
-	struct s_memory	*prev;
-	size_t			size;
-	bool			isfree;
-	struct s_memory	*next;
-}				t_memory;
+	void	*addr;
+	size_t	size;
+	bool	isfree;
+}				t_info;
 
-# define ZONE_CAPACITY
+# define ZONE_CAPACITY (100)
 
 # define TINY_MIN (1)
 # define TINY_MAX (256)
-# define TINY_ZONE (ZONE_CAPACITY * TINY_MAX)
+# define TINY_ZONE_SIZE (ZONE_CAPACITY * TINY_MAX)
+# define TINY_MAX_CAPACITY (TINY_ZONE_SIZE / TINY_MIN)
 
 # define SMALL_MIN (TINY_MAX + 1)
-# define SMALL_MAX 1024
-# define SMALL_ZONE (ZONE_CAPACITY * SMALL_MAX)
+# define SMALL_MAX (1024)
+# define SMALL_ZONE_SIZE (ZONE_CAPACITY * SMALL_MAX)
+# define SMALL_MAX_CAPACITY (SMALL_ZONE_SIZE / SMALL_MIN)
 
 # define LARGE_MIN (SMALL_MAX + 1)
 
--- rendu/src/free/free.c
+++ rendu/src/free/free.c
@@ -4,4 +4,13 @@ void	free(void *ptr)
 {
 	if (ptr == NULL)
 		return ;
+	/*
+	find corresponding info
+	info->isfree = 1;
+	merge
+	*/
+	/*
+	if (empty zone)
+		mmunmap(zone, zone size);
+	*/
 }
