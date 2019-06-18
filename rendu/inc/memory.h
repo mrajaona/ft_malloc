579eaeb96915c8ca5dfbf578bc4f7e9e36a0d3e4
Maryse RAJAONARY <mrajaona@e3r5p9.42.fr>
2019-06-18 10:59:50 +0200

    ...

--- rendu/inc/memory.h
+++ rendu/inc/memory.h
@@ -19,12 +19,10 @@ typedef struct	s_info
 # define TINY_MIN (1)
 # define TINY_MAX (256)
 # define TINY_ZONE_SIZE (ZONE_CAPACITY * TINY_MAX)
-# define TINY_MAX_CAPACITY (TINY_ZONE_SIZE / TINY_MIN)
 
 # define SMALL_MIN (TINY_MAX + 1)
 # define SMALL_MAX (1024)
 # define SMALL_ZONE_SIZE (ZONE_CAPACITY * SMALL_MAX)
-# define SMALL_MAX_CAPACITY (SMALL_ZONE_SIZE / SMALL_MIN)
 
 # define LARGE_MIN (SMALL_MAX + 1)
 
