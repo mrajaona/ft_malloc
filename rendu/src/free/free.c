072676d3ad5147975e0ce254543d1d527bf0645b
Maryse RAJAONARY <mrajaona@e3r4p9.42.fr>
2019-06-25 11:44:48 +0200

    debut ok

--- rendu/src/free/free.c
+++ rendu/src/free/free.c
@@ -12,20 +12,45 @@ static void	free_large(t_elem_info *elem)
 		write(2, "munmap failed\n", 14);
 }
 
-static t_elem_info	*find_elem(void *ptr)
+static t_elem_info	*find_in_zone(t_elem_info *first, const void *ptr)
 {
 	t_elem_info	*elem;
 
-	// LARGE
-	elem = g_zones.large;
+	elem = first;
 	while (elem)
 	{
 		if (elem->addr == ptr)
 			return (elem);
 		elem = elem->next;
 	}
-
 	return (NULL);
+}
+
+static t_elem_info	*find_elem(const void *ptr)
+{
+	t_zone_info	*zone;
+	t_elem_info	*elem;
+
+	// TINY
+	zone = g_zones.tiny;
+	while (zone)
+	{
+		if (NULL != (elem = find_in_zone(zone->first, ptr)))
+			return (elem);
+		zone = zone->next;
+	}
+
+	// SMALL
+	zone = g_zones.small;
+	while (zone)
+	{
+		if (NULL != (elem = find_in_zone(zone->first, ptr)))
+			return (elem);
+		zone = zone->next;
+	}
+
+	// LARGE
+	return (find_in_zone(g_zones.large, ptr));
 }
 
 void	free(void *ptr)
