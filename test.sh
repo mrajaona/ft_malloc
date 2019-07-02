4b584d5aa779d600839eaaf576f1025ff3df1be2
Maryse RAJAONARY <mrajaona@e1r6p6.42.fr>
2019-07-02 09:44:11 +0200

    script

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 
 clear
 
+make -C rendu re
+make -C rendu clean
+
 rm -f *.so
 cp ./rendu/*.so .
 rm -f ./inc/*
@@ -9,14 +12,28 @@ cp ./rendu/ft_malloc.h ./inc
 
 gcc -Wall -Wextra -Werror -I./inc/ -L. -lft_malloc -o test main.c
 
-echo "------------ system"
+if [ $# -eq 0 ]
 
-./test
+	then
+	echo "------------ system"
 
-echo "------------ custom"
+elif [ $1 = "macos" ]
 
-export DYLD_LIBRARY_PATH=.
-export DYLD_INSERT_LIBRARIES="libft_malloc.so"
-export DYLD_FORCE_FLAT_NAMESPACE=1
+	then
+	echo "------------ macos"
+
+	export DYLD_LIBRARY_PATH=.
+	export DYLD_INSERT_LIBRARIES="libft_malloc.so"
+	export DYLD_FORCE_FLAT_NAMESPACE=1
+
+elif [ $1 = "linux" ]
+
+	then
+	echo "------------ linux"
+
+	export LD_LIBRARY_PATH=.
+	export LD_PRELOAD="libft_malloc.so"
+
+fi
 
 ./test
