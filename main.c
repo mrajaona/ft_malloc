9bc3ca2351fbab080f5ec23c169a44fe22bf8c55
maryse <rajaon_m@naver.com>
2019-07-05 08:55:42 +0200

    realloc foireux

--- main.c
+++ main.c
@@ -1,39 +1,132 @@
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <pthread.h>
 
 #include "ft_malloc.h"
 
+#define SIZE_T 200
+#define SIZE_S 500
+#define SIZE_L 1050
+
+/*
+pthread_t tid[2];
+int counter;
+pthread_mutex_t lock;
+
+void* doSomeThing(void *arg)
+{
+	void *a = NULL;
+	void *b = NULL;
+	void *c = NULL;
+
+	a = malloc(1 << 6);
+	b = malloc(0);
+	c = malloc(1 << 11);
+
+	if (!a) write(1, "a is NULL\n", 10);
+	if (!b) write(1, "b is NULL\n", 10);
+	if (!c) write(1, "c is NULL\n", 10);
+
+	show_alloc_mem();
+	write(1, "1 --\n", 5);
+
+	free(a); a = NULL;
+	free(b); b = NULL;
+	free(c); c = NULL;
+
+	show_alloc_mem();
+	write(1, "2 --\n", 5);
+	(void)arg;
+	return NULL;
+}
+*/
+
 int main(void)
 {
-	char	*tmp[10];
-	int		i;
+/*
+	int i = 0;
 
-	i = 0;
-	while (i < 10)
+	if (pthread_mutex_init(&lock, NULL) != 0)
+		return 1;
+
+	while(i < 2)
 	{
-		// tmp[i] = malloc(1025);
-		// tmp[i] = malloc(7);
-		tmp[i] = malloc(100);
-		if (tmp[i])
+		pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
+		i++;
+	}
+
+	pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
+	pthread_mutex_destroy(&lock);
+*/
+//    doSomeThing(NULL);
+
+	{
+		size_t	tab[] = {SIZE_T, SIZE_S, SIZE_L};
+		char	*tmp[10];
+		int		i;
+
+		i = 0;
+		while (i < 10)
 		{
-			strcpy(tmp[i], "test\n");
-			write(1, tmp[i], 5);
+			tmp[i] = malloc(tab[i % 3]);
+			if (tmp[i])
+			{
+				strcpy(tmp[i], "test\n");
+				write(1, tmp[i], 5);
+			}
+			else
+				write(1, "not allocated\n", 14);
+			i++;
 		}
-		else
-			write(1, "not allocated\n", 14);
-		i++;
+
+		write(1, "malloc\n", 7);
+		show_alloc_mem();
+		write(1, "\n", 1);
+
+		i = 0;
+		while (i < 10)
+		{
+			tmp[i] = realloc(tmp[i], SIZE_T);
+			i++;
+		}
+
+		write(1, "realloc\n", 8);
+		show_alloc_mem();
+		write(1, "\n", 1);
+
+		i = 0;
+		while (i < 10)
+		{
+			free(tmp[i]);
+			i++;
+		}
+
+		write(1, "free\n", 5);
+		show_alloc_mem();
+		write(1, "\n", 1);
 	}
 
-	show_alloc_mem();
+	write(1, "\n", 1);
 
-	i = 0;
-	while (i < 10)
 	{
-		free(tmp[i]);
-		i++;
+		char	*tmp;
+
+		tmp = malloc(7);
+		
+		write(1, "malloc\n", 7);
+		show_alloc_mem();
+		write(1, "\n", 1);
+		
+		*tmp = 'a';
+		write(1, tmp, 1);
+		write(1, "\n", 1);
+		free(tmp);
+		
+		write(1, "free\n", 5);
+		show_alloc_mem();
+		write(1, "\n", 1);
 	}
 
-	show_alloc_mem();
-
-	return (0);
+	return 0;
 }
