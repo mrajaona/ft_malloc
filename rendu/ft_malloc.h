d935add1b2941cc27a7bbeb45a4620bd148f66ca
Maryse RAJAONARY <mrajaona@e1r4p6.42.fr>
2019-07-08 13:27:02 +0200

    norme

--- rendu/ft_malloc.h
+++ rendu/ft_malloc.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrajaona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/08 13:26:24 by mrajaona          #+#    #+#             */
+/*   Updated: 2019/07/08 13:26:25 by mrajaona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_MALLOC_H
 # define FT_MALLOC_H
 
 # include <stddef.h>
 
-void    free(void *ptr);
-void    *malloc(size_t size);
-void    *realloc(void *ptr, size_t size);
-void    show_alloc_mem(void);
+void	free(void *ptr);
+void	*malloc(size_t size);
+void	*realloc(void *ptr, size_t size);
+void	show_alloc_mem(void);
 
 #endif
